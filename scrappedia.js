2cadd9408ba82d2f11467ee8d4f75b3a0565546b
chasni <achmad.ibnu@bukalapak.com>
2024-07-05 03:02:54 +0700

    [ACH-1] Add price logic

--- scrappedia.js
+++ scrappedia.js
@@ -1,6 +1,6 @@
 const { chromium } = require('playwright');
 
-async function scrapeTokopedia(url) {
+async function scrapeTokopedia(url, maxPrice) {
     const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
@@ -12,10 +12,16 @@ async function scrapeTokopedia(url) {
         console.log('Loading Tokopedia Detail Page...');
         await page.waitForSelector('.price', { timeout: 60000 });
 
-        const productPrice = await page.$$eval('.price', prices => {
+        const priceText = await page.$$eval('.price', prices => {
             return prices.map(price => price.innerText);
         });
-        console.log('Product Price: ', productPrice[0]);
+        const price = parseFloat(priceText[0].replace(/[^\d.,]/g, '').replace(/[,.]/g, ''));
+
+        if (price && price > maxPrice) {
+            console.log('You cannot afford this, work more!');
+        } else {
+            console.log('It is cheap! Buy now!');
+        }
     } catch (error) {
         console.error('Error:', error);
     } finally {
@@ -25,7 +31,8 @@ async function scrapeTokopedia(url) {
 
 // TODO : use input? 
 const url = 'https://www.tokopedia.com/mitrahokicomindo1gg/mouse-gaming-logitech-g604-lightspeed-wireless-gaming-mouse';
-scrapeTokopedia(url)
+const maxPrice = 5000000
+scrapeTokopedia(url, maxPrice)
     .then(() => {
         console.log('Scraping completed successfully.');
     })
