b833f298fc66a903df843d81c12abfd83d356aed
chasni <achmad.ibnu@bukalapak.com>
2024-07-05 14:56:02 +0700

    [ACH-4] Create headless scrapper (Detik)

---
+++ detik.js
@@ -0,0 +1,26 @@
+const { chromium } = require('playwright');
+
+(async () => {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+
+    try {
+        await page.goto('https://www.detik.com/', { waitUntil: 'domcontentloaded' });
+
+        await page.waitForSelector('h2.media__title a.media__link');
+
+        const mainNews = await page.$$eval('h2.media__title a.media__link', links => {
+            return links.map(link => {
+                const title = link.getAttribute('dtr-ttl');
+                const href = link.getAttribute('href');
+                return { title, href };
+            });
+        });
+
+        console.log('Main News:', mainNews);
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        await browser.close();
+    }
+})();
